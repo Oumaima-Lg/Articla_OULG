c74b1d67b26b3b44f86d4625a0499a167b5f9f54
Oumaima-Lg <laghjibioumaima2003@gmail.com>
2025-06-16 13:04:30 +0100

    bad lat

--- src/pages_details/articlaPage/Home.jsx
+++ src/pages_details/articlaPage/Home.jsx
@@ -11,13 +11,15 @@ import { IoCreateOutline } from "react-icons/io5";
 import { PiArchiveLight } from "react-icons/pi";
 import ShinyText from '../../blocks/TextAnimations/ShinyText/ShinyText';
 import { Link as LinkRouter } from "react-router-dom";
+import { BiCollapseHorizontal } from "react-icons/bi";
+import Switch from '../../semiComponents/SideButton';
 
 import Spline from '@splinetool/react-spline';
 
 export function AppSpline() {
-  return (
-    <Spline scene="https://prod.spline.design/VbFDnYNrBVOXKv9u/scene.splinecode" />
-  );
+    return (
+        <Spline scene="https://prod.spline.design/VbFDnYNrBVOXKv9u/scene.splinecode" />
+    );
 }
 
 
@@ -26,15 +28,15 @@ export function AppSpline() {
 const Home = () => {
     return (
 
-        <div className='flex mx-auto lg:pt-34 pt-22 min-h-screen bg-gradient-to-bl from-[#171717] from-55% to-[#4E3F59] to-100%'>
-            <div className=' w-xs pt-12 '>
+        <div className='flex sm:flex-row flex-col   mx-auto lg:pt-34 pt-22 min-h-screen bg-gradient-to-bl from-[#171717] from-55% to-[#4E3F59] to-100%'>
+            <div className=' w-xs pt-12 sm:block hidden'>
                 <div className=' border-x-3 border-b-3 px-4  border-[#202020]  md:h-3/4 h-2/3 md:w-3/4 w-2/3 bg-gradient-to-br from-[#171717] from-54% to-[#4E3F59] to-100%  rounded-lg shadow-lg    text-white flex flex-col justify-between  py-20 '>
 
                     <div className='flex flex-row items-center gap-3 cursor-pointer hover:text-[#A09F87] hover:scale-105 transform-gpu' >
                         <div>
                             <PiArticleLight className='md:text-5xl text-2xl' />
                         </div>
-                        <LinkRouter to = '/articla/article' className='font-bold md:text-xl text-sm'>
+                        <LinkRouter to='/articla/article' className='font-bold md:text-xl text-sm'>
                             Article
                         </LinkRouter>
                     </div>
@@ -56,7 +58,41 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <div className='  w-5/6 flex flex-col  gap-6 lg:gap-10 '>
+
+            <div className='sm:hidden flex items-center justify-center pt-20 pb-10'>
+                <Switch />
+            </div>
+            <div className=' fixed bottom-0 top-30 left-0 right-0'>
+                <div className=' border-x-3 border-b-3 px-4  border-[#202020]  md:h-3/4 h-2/3 md:w-3/4 w-2/3 bg-gradient-to-br from-[#171717] from-54% to-[#4E3F59] to-100%  rounded-lg shadow-lg    text-white flex flex-col justify-between  py-20 '>
+
+                    <div className='flex flex-row items-center gap-3 cursor-pointer hover:text-[#A09F87] hover:scale-105 transform-gpu' >
+                        <div>
+                            <PiArticleLight className='md:text-5xl text-2xl' />
+                        </div>
+                        <LinkRouter to='/articla/article' className='font-bold md:text-xl text-sm'>
+                            Article
+                        </LinkRouter>
+                    </div>
+                    <div className='flex flex-row items-center gap-3 cursor-pointer  hover:text-[#A09F87] hover:scale-105 transform-gpu' >
+                        <div>
+                            <IoCreateOutline className='md:text-5xl text-2xl' />
+                        </div>
+                        <div className='font-bold md:text-xl text-sm'>
+                            Nouveau <br /> Article
+                        </div>
+                    </div>
+                    <div className='flex flex-row items-center gap-3 cursor-pointer  hover:text-[#A09F87] hover:scale-105 transform-gpu' >
+                        <div>
+                            <PiArchiveLight className='md:text-5xl text-2xl' />
+                        </div>
+                        <div className='font-bold md:text-xl text-sm'>
+                            Vos articles <br /> personnels
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='w-full  sm:w-5/6 flex flex-col  gap-6 lg:gap-10 '>
                 <div className='pt-5 text-center  text-gradient charm xl:text-6xl text-3xl leading-tight '>
                     {/* BIENVENUE OUMAIMA LAGHJIBI */}
                     <ShinyText text=" BIENVENUE OUMAIMA LAGHJIBI" disabled={false} speed={3} className='' />
@@ -85,8 +121,6 @@ const Home = () => {
                 </div>
             </div>
         </div>
-
-
     )
 }
 
